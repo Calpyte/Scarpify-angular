8b0eeaf00d7f5df37f046257f777615210b24a8c
Dhinesh <dhineshdinu201@gmail.com>
2023-05-14 20:31:26 +0530

    left bar changes

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { LeftBarComponent } from './left-bar/left-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    TopBarComponent
+    TopBarComponent,
+    LeftBarComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [TopBarComponent]
+  exports: [TopBarComponent, LeftBarComponent]
 })
 export class CommonSharedModule { }
