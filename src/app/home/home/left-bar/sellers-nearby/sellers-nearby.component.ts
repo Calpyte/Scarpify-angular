890bbd717f8b297fa9561dbf32dbcde6f78d6d32
DESKTOP-UR9EK4O\Calpyte <dineshspm18@gmail.com>
2023-05-19 17:12:26 +0530

    main page side bar completed

--- src/app/home/home/left-bar/sellers-nearby/sellers-nearby.component.ts
+++ src/app/home/home/left-bar/sellers-nearby/sellers-nearby.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConsumerDetailsComponent } from '../../consumer-details/consumer-details.component';
+import { DetailDialogComponent } from '../detail-dialog/detail-dialog.component';
 
 @Component({
   selector: 'app-sellers-nearby',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellersNearbyComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+  sellers: any = [
+    {
+      id: 0,
+      name: 'Dinesh'
+    },
+    {
+      id: 1,
+      name: 'Kumar'
+    },
+    {
+      id: 2,
+      name: 'Kishore'
+    }
+  ]
 
   ngOnInit() {
+
+  }
+
+  openDialog(user): void {
+    const dialogRef = this.dialog.open(DetailDialogComponent, {
+      data: user,
+      hasBackdrop: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      // alert(JSON.stringify(result));
+    });
+  }
+
+  openDetail = (user) => {
+    this.openDialog(user);
   }
 
 }
