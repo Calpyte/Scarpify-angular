0f222714acbf2dc347236c2334562812792550af
DESKTOP-UR9EK4O\Calpyte <dineshspm18@gmail.com>
2023-05-22 16:45:34 +0530

    latest changes

--- src/app/transaction/quantity-details/quantity-details.component.ts
+++ src/app/transaction/quantity-details/quantity-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-quantity-details',
@@ -8,7 +9,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class QuantityDetailsComponent implements OnInit {
   isViewDetail: boolean = false;
   @Input() data: any;
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public data2: any,) { }
 
   ngOnInit() {
   }
