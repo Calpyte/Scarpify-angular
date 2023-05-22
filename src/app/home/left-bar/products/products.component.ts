a1456e495d1c1ea6d3e091c921af3509b36107ea
Yuvaraj <yuvaraj@calpyte.com>
2023-05-22 21:12:19 +0530

    select-reasons popUp added

--- src/app/home/left-bar/products/products.component.ts
+++ src/app/home/left-bar/products/products.component.ts
@@ -7,6 +7,7 @@ import { HttpsApiService } from 'src/app/https-api.service';
 import { ProductDetailsComponent } from 'src/app/transaction/product-details/product-details.component';
 import { QuantityDetailsComponent } from 'src/app/transaction/quantity-details/quantity-details.component';
 import { RatingComponent } from 'src/app/transaction/rating/rating.component';
+import { SelectReasonsComponent } from 'src/app/transaction/select-reasons/select-reasons.component';
 
 
 
@@ -34,7 +35,7 @@ export class ProductsComponent implements OnInit {
 
   openDialog(user): void {
     this.dialog.closeAll();
-    const dialogRef = this.dialog.open(user.toLowerCase() === 'paper' ? QuantityDetailsComponent : RatingComponent, {
+    const dialogRef = this.dialog.open(user.toLowerCase() === 'paper' ? QuantityDetailsComponent : SelectReasonsComponent, {
       data: user,
       hasBackdrop: true,
       disableClose: true
