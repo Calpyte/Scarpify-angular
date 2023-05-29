b3af0f7acd63a7d35c6834d5cb109e655cd20c1d
Yuvaraj <yuvaraj@calpyte.com>
2023-05-29 15:01:01 +0530

    rating latest changes

--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionComponent } from './transaction.component';
 import { ContactUsComponent } from '../referral/contactUs/ContactUs.component';
+import { NgxStarRatingModule } from 'ngx-star-rating';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgxStarRatingModule
   ],
   declarations: [TransactionComponent]
 })
